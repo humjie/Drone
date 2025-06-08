6c862307742ff75166d8a1aab1f9b678fcc4ebc4
zy <czy3k1@gmail.com>
2025-06-08 13:53:36 +0800

    Add files via upload

--- ros2_ws/build/adxl345_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/orangepi/ros2_ws/src/adxl345_node
-# Build directory: /home/orangepi/ros2_ws/build/adxl345_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
